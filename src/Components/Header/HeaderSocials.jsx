d556c16af4816992c0d3ff3fcf6faa17fdf078bc
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2022-03-04 22:09:15 +0100

    changes

--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 const headerSocials = () => {
     return (
         <div className='header_socials'>
-            <a href="https://www.linkedin.com/in/ekunolajohn/" target="_blank"><BsLinkedin/></a>
-            <a href="https://github.com/Jizzyjay" target="_blank"><BsGithub /></a>
+            <a href="https://www.linkedin.com/in/ekunolajohn/" rel="noreferrer" target="_blank"><BsLinkedin/></a>
+            <a href="https://github.com/Jizzyjay" rel="noreferrer" target="_blank"><BsGithub /></a>
         </div>
     )
 }
