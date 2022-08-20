8af3520698caaf21a24f483f9b08094546002843
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2022-08-20 20:10:32 +0100

    rearranged project

--- src/Components/Projects/project.jsx
+++ src/Components/Projects/project.jsx
@@ -20,16 +20,6 @@ const project = () => {
             <div className='container project_container'>
             <article className='project_item'>
                 <div className='project_item-image'></div>
-                <h3>Roboapp</h3>
-                <div className='project_item-cta'>
-                    <a href='https://github.com/Jizzyjay/roboapp' rel="noreferrer" target='_blank' className='btn'>Github</a>
-                    <a href='https://robopp.netlify.app/' rel="noreferrer" target='_blank' className='btn btn-primary'>Live Demo </a>
-                </div>
-                <img src={Robo} alt="Robo app" />
-                <h3>Roboapp is a robot app, where robots are generated with an API.</h3>
-            </article>
-            <article className='project_item'>
-                <div className='project_item-image'></div>
                 <h3>LearnCode</h3>
                 <div className='project_item-cta'>
                     <a href='https://github.com/Jizzyjay/learncode' rel="noreferrer" target='_blank' className='btn'>Github</a>
@@ -89,6 +79,16 @@ const project = () => {
                 <h3>9ja FoodHub landing page </h3>
             </article>
             <article className='project_item'>
+                <div className='project_item-image'></div>
+                <h3>Roboapp</h3>
+                <div className='project_item-cta'>
+                    <a href='https://github.com/Jizzyjay/roboapp' rel="noreferrer" target='_blank' className='btn'>Github</a>
+                    <a href='https://robopp.netlify.app/' rel="noreferrer" target='_blank' className='btn btn-primary'>Live Demo </a>
+                </div>
+                <img src={Robo} alt="Robo app" />
+                <h3>Roboapp is a robot app, where robots are generated with an API.</h3>
+            </article>
+            <article className='project_item'>
                 <div className='project_item-img'></div>
                 <h3>Jay Bakery</h3>
                 <div className='project_item-cta'>
@@ -98,15 +98,16 @@ const project = () => {
                 <img src={Jay} alt="Jay img" />
                 <h4>Jay Bakery landing page shows the products, price, location and services offered by them.</h4>
             </article>
-            <article className='project_item'>
+
+            {/* <article className='project_item'>
                 <div className='project_item-image'></div>
                 <h3>Todo App</h3>
                 <a href='https://github.com/Jizzyjay/To-do-App' rel="noreferrer" target='_blank' className='btn'>Github</a>
                 <a href='https://todooaapp.netlify.app/' rel="noreferrer" target='_blank' className='btn btn-primary'>Live Demo</a>
                 <img src={Todo} alt="Todo" />
                 <h4>ToDo App is designed to keep track and maintain our day-to-day tasks or list everything that we have to do. It's helpful in planning our daily schedules. We can add more tasks at any time and delete a task that is completed.</h4>
-            </article>
-            <article className='project_item'>
+            </article> */}
+            {/* <article className='project_item'>
                 <div className='project_item-image'></div>
                 <h3>Background Gradient</h3>
                 <div className='project_item-cta'>
@@ -115,7 +116,7 @@ const project = () => {
                 </div>
                 <img src={Background} alt="Jay" />
                 <h4>This color gradient specifies a range of position-dependent colors. The colors produced by a gradient vary continuously with position, producing smooth color transitions.</h4>
-            </article>  
+            </article>   */}
             <article className='project_item'>
                 <div className='project_item-image'></div>
                 <h3>Tasty Dish Landing Page</h3>
